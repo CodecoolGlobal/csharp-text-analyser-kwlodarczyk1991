6d41670689d30cc1c9d91ae3901fa1947c46a0af
Krzysiek <Krzysiek@DESKTOP-QPN7HML>
2020-05-17 15:05:33 +0200

    first commit

---
+++ Application.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public class Application
+{
+	public static void main(string[] args)
+    {
+        Console.WriteLine("doopa");
+    }
+	
+	
+}
